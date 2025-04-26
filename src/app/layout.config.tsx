549812a09afea6eec58f17003eb82907460e7104
ShadowArcanist <projectshadowmoon@gmail.com>
2025-04-26 23:23:35 +0530

    Added Content pages for /home

--- src/app/layout.config.tsx
+++ src/app/layout.config.tsx
@@ -1,4 +1,4 @@
-import { BookIcon, CodeIcon, HelpCircleIcon } from 'lucide-react';
+import Image from 'next/image';
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 
 /**
@@ -12,14 +12,7 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
+        <Image src="/logo.png" alt="Logo" width={24} height={24} />
         Coolify Documentation
       </>
     ),
