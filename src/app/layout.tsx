dfbf719ec8b238eb9310c2da17b0dbbf0286307b
ShadowArcanist <projectshadowmoon@gmail.com>
2025-04-26 20:33:59 +0530

    Added Banner to show announcements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
-
+import { Banner } from 'fumadocs-ui/components/banner';
 const inter = Inter({
   subsets: ['latin'],
 });
@@ -11,6 +11,7 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
+      <Banner id="v5.0.0-beta.412" variant="rainbow">🚀 Coolify v5.0.0-beta.412 is now released to Public! </Banner>
       <RootProvider
           search={{
             options: {
