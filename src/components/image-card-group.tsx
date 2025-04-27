3aee4b79b412332e95a64d126a2423c3efa34369
ShadowArcanist <projectshadowmoon@gmail.com>
2025-04-27 15:33:26 +0530

    Refactored + Improved image-card-group component

--- src/components/image-card-group.tsx
+++ src/components/image-card-group.tsx
@@ -1,10 +1,10 @@
 import { ReactNode } from "react";
 
-interface ImageCardsProps {
+interface ImageCardGroupProps {
   children: ReactNode;
 }
 
-export function ImageCards({ children }: ImageCardsProps) {
+export function ImageCardGroup({ children }: ImageCardGroupProps) {
   return (
     <div className="grid grid-cols-1 gap-6 sm:grid-cols-3 mt-5">
       {children}
