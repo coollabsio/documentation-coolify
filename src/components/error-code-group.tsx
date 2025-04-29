78ce51a6235e5f145db68d00d7b016e7171f008a
ShadowArcanist <projectshadowmoon@gmail.com>
2025-04-29 13:41:16 +0530

    Updated error-code component to look better visually

--- src/components/error-code-group.tsx
+++ src/components/error-code-group.tsx
@@ -6,7 +6,7 @@ interface ErrorCodeGroupProps {
 
 export function ErrorCodeGroup({ children }: ErrorCodeGroupProps) {
   return (
-    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 mt-2">
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 mt-2">
       {children}
     </div>
   );
--- src/components/error-code.tsx
+++ src/components/error-code.tsx
@@ -11,7 +11,7 @@ type ErrorCodeProps = Omit<HTMLAttributes<HTMLDivElement>, 'title'> & {
 export const ErrorCode = forwardRef<HTMLDivElement, ErrorCodeProps>(
   ({ className, children, title, code, href, ...props }, ref) => {
     const statusCode = code ? (
-      <span className="ml-2 inline-flex items-center justify-center bg-[#ffe4e4] dark:bg-[#573131] text-[#ff6166] dark:text-[#ff6166] rounded-lg text-sm font-medium mr-3 h-8 w-12">
+      <span className="ml-5 inline-flex items-center justify-center bg-[#ffe4e4] dark:bg-[#3c2525] text-[#ff6166] dark:text-[#ff6166] rounded-lg text-sm font-medium mr-3 h-8 w-12">
         {code}
       </span>
     ) : null;    
@@ -25,7 +25,7 @@ export const ErrorCode = forwardRef<HTMLDivElement, ErrorCodeProps>(
     const cardContent = (
       <div
         className={cn(
-          'flex flex-row items-center gap-1 rounded-lg bg-fd-card p-3 text-sm text-fd-card-foreground shadow-md transition hover:shadow-lg hover:bg-fd-muted cursor-pointer overflow-hidden',
+          'flex flex-row items-center gap-1 rounded-lg bg-fd-card p-1 text-sm text-fd-card-foreground shadow-md transition hover:shadow-lg hover:bg-fd-muted cursor-pointer overflow-hidden',
           className,
         )}
         {...props}
