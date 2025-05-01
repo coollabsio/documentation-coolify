6df2f9dc4c9af39df3d0e102914054b162f946ce
ShadowArcanist <projectshadowmoon@gmail.com>
2025-05-01 11:11:10 +0530

    Added Robots.txt to block all indexing until v5 is released to public

--- apps/docs-site/next-sitemap.config.js
+++ apps/docs-site/next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://coolify.io/docs', // Replace with your domain
-  generateRobotsTxt: true,        // Generate a robots.txt along with your sitemap
+  generateRobotsTxt: false,        // Generate a robots.txt along with your sitemap
   // Optional settings:
   changefreq: 'daily',
   priority: 0.7,
